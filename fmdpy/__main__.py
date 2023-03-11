1952a0c7792b031a95e590740228555c5fc5c4bc
Alok Saboo <arsaboo@gmx.com>
2023-03-11 16:39:30 -0500

    Update __main__.py

--- fmdpy/__main__.py
+++ fmdpy/__main__.py
@@ -106,8 +106,9 @@ def fmdpy(count, search, fmt, bitrate, multiple,
     (-b is ignored)
     """
     search = ' '.join(search)
+    print(search)
     if 'jiosaavn.com/song/' in search:
-        print(search)
+        print("search is {}. ".format(search))
         get_song_id(search)
         sys.exit(0  )
     if 'spotify.com/playlist' in search:
