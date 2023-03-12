b5fe55ea1738f0eaa1757df3bbb9563d84dd4c94
Alok Saboo <arsaboo@gmx.com>
2023-03-11 21:28:30 -0500

    Update api.py

--- fmdpy/api.py
+++ fmdpy/api.py
@@ -32,9 +32,7 @@ def parse_query(query_json):
 def get_song_url(url):
     """Get song_id from url."""
     data = asyncio.run(saavn.get_song_details(url))
-    sng = parse_song_url(data)
-    print(sng)
-    return(sng)
+    return(parse_song_url(data))
 
 def parse_song_url(data):
     """Get song_id from url."""
